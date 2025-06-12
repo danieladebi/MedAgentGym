5cf6b8e54599814d3ba359cab359d87834d510a8
Ikechukwu Adebi <ikadebiutsw@c301-001.ls6.tacc.utexas.edu>
2025-06-11 19:32:34 -0500

    added more framework code

--- ehr_gym/env/task/medxpertqa.py
+++ ehr_gym/env/task/medxpertqa.py
@@ -2,4 +2,57 @@ from .base import AbstractEHRTask
 
 class MedXpertQATask(AbstractEHRTask):
     # TODO: complete this
-    pass
+    permitted_actions = ['request_info', 'validate_code', 'debug']
+    def __init__(
+        self,
+        task_id: int,
+        data_path: str = None,
+        debugger_config: dict = None,
+        mode: str = "test",
+    ) -> None:
+        super().__init__(task_id=task_id)
+        self.task_id = task_id
+        self.task_list = None
+        self.data_path = data_path
+        self.debugger_config = debugger_config
+        self.mode = mode
+
+    @classmethod
+    def get_task_id(cls):
+        # Get the class name and remove the word 'Task' from the end if it exists
+        class_name = cls.__name__.replace("Task", "")
+        # Convert CamelCase to hyphen-separated format
+        formatted_name = "".join(
+            ["-" + c.lower() if c.isupper() else c for c in class_name]
+        ).lstrip("-")
+        return f"EHRGym.medxpertqa.{formatted_name}"
+
+    def setup(self) -> tuple[str, dict]:
+        """
+        Set up the task
+
+        Parameters:
+        -----------------
+        data_path: str
+            Path to the data directory
+        """
+        raise NotImplementedError
+
+     def setup_goal(self) -> tuple[str, dict]:
+        """
+        Set up the goal and info for the task
+
+        Parameters:
+        -----------------
+        data_path: str
+            Path to the data directory
+        """
+        raise NotImplementedError
+        
+    def _get_obs(self) -> dict:
+        raise NotImplementedError
+        
+    
+
+
+    
